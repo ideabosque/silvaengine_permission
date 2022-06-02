e963418367bf7eec563790f3413c8eb5d87ec2a7
Barry Liu <barry.y.liu@hotmail.com>
2022-06-02 12:48:41 +0800

    [U] Updated

--- silvaengine_permission/permission/handlers.py
+++ silvaengine_permission/permission/handlers.py
@@ -763,20 +763,13 @@ def get_users_by_role_type(
     #         ]
     #     )
     # )
-    relationships = Utility.json_loads(
-        Utility.json_dumps(
-            [
-                relationship
-                for relationship in RelationshipModel.apply_to_type_index.query(
-                    hash_key=str(channel).strip(),
-                    range_key_condition=(
-                        RelationshipModel.type == int(relationship_type)
-                    ),
-                    # filter_condition=relationship_filter_condition,
-                )
-            ]
-        )
+    results = RelationshipModel.apply_to_type_index.query(
+        hash_key=str(channel).strip(),
+        range_key_condition=(RelationshipModel.type == int(relationship_type)),
+        # filter_condition=relationship_filter_condition,
     )
+    test = [relationship for relationship in results]
+    relationships = Utility.json_loads(Utility.json_dumps(test))
 
     print(">>>>>>>>>>>>>>> Get relationships: {}".format(t() - s))
     s = t()
